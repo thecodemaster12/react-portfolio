52c5d0dd01f712b47bc1f2aa61fefa0c74fee5dc
Saifur Rahman Shihab <thecodemaster26@gmail.com>
2024-04-27 16:35:08 +0600

    Vite project done with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-700 text-center'>Portfolio</h1>
+    </>
+  )
+}
+
+export default App

12c9f266a9ee016d139486e5a98b5951149428c8
Saifur Rahman Shihab <thecodemaster26@gmail.com>
2024-05-08 17:48:07 +0600

    Fixed image src problem

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import ProjectCard from "./ProjectCard";
+import project1 from '../images/project1.png'
+import project2 from '../images/project2.png'
+import project3 from '../images/project3.png'
 import Footer from "./Footer";
 
 const Projects = () => {
@@ -12,19 +15,19 @@ const Projects = () => {
         {/* <div className="flex gap-5 p-10 justify-center flex-wrap"> */}
         <div className="p-6 grid justify-center grid-cols-1 gap-10 md:gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <ProjectCard 
-          imgPath='/project3.png'
+          imgPath={project3}
           title='Ramadan Calender'
           description='This user-friendly Ramadan calendar keeps you informed throughout the holy month.'
           projectUrl='https://ramadanbd.netlify.app/'
           />
         <ProjectCard 
-        imgPath='/project1.png'
+        imgPath={project1}
         title='Mortgage Calculator'
         description='This interactive tool helps you estimate mortgage costs and plan your homeownership journey.'
         projectUrl='https://mortgage-cal.netlify.app/'
         />
         <ProjectCard 
-        imgPath='/project2.png'
+        imgPath={project2}
         title='Rick & Morty Wiki'
         description='This comprehensive wiki catalogs every character from the wacky animated series.'
         projectUrl='https://rick-and-morty-wikis.netlify.app/'
